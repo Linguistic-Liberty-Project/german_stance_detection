64c89eb62a7584e31e3a26c6e3144b62d0a188ce
LidiiaMelnyk95 <73523072+LidiiaMelnyk95@users.noreply.github.com>
2022-02-28 16:27:04 +0100

    Glove stance detection
    testing different classifiers with glove embeddings

--- main.py
+++ main.py
@@ -23,11 +23,14 @@ from nltk.stem.porter import PorterStemmer
 import warnings
 warnings.filterwarnings("ignore")
 
+
+#function to get the labels
 def labelStance(labelDict, data):
 	for key, val in labelDict.items():
-		data.loc[data["Stance"] == val, "Stance"] = int(key)
+		data.loc[data["Label"] == val, "Label"] = int(key)
 	return data
 
+#create a dictionary of glove vectors from the file
 def readGlobalVecData(glove_word_vec_file):
 	file = open(glove_word_vec_file, encoding="utf8")
 	rawData = file.readlines()
@@ -42,16 +45,146 @@ def readGlobalVecData(glove_word_vec_file):
 gloveFile = "/Users/lidiiamelnyk/Downloads/vectors.txt"
 
 print("\nLoading Glove data in progress...")
+#read glove vectors with the function
 glove_word_vec_dict = readGlobalVecData(gloveFile)
 print("\nLoading Glove data is done...")
 
+#load classifiers
 classifiers = ['Support Vector Machine', 'Random Forest Classifier', 'Gradient Boosting Classifier',  'K Neighbors Classifier', 'Decision Tree Classifier']
 
 training = "/Users/lidiiamelnyk/Documents/trans_train.csv"
 
-Comments = pd.read_csv(training,encoding='utf-8')
-
+Comments = pd.read_csv(training,encoding='utf-8', sep = ';')
+Comments = Comments.dropna(axis = 0, how = 'any', inplace = False)
 #For converting all the stances into numerical values in both training and test data
 labelDict = {0:"Against", 1:"Favor", 2:"Neutral"}
 Comments = labelStance(labelDict, Comments)
 print('Shape of label tensor:', Comments.shape)
+
+#get glove vectors if word in list
+def getWordVector(word, glove_word_vec_dict):
+	if word in glove_word_vec_dict:
+		return glove_word_vec_dict[word]
+	return np.zeros_like(glove_word_vec_dict['dummy'])
+
+def sumVectors(finalList, glove_word_vect_dict):
+	numNonZero = 0
+	#get dummies
+	vector = np.zeros_like(glove_word_vec_dict['dummy'])
+	for word in finalList:
+		vect = getWordVector(word, glove_word_vec_dict)
+		if vect.sum() != 0:
+			vector += vect
+			numNonZero += 1
+
+	if numNonZero:
+		vector = vector/numNonZero
+	return vector
+
+def sumVectorsCNN(finalList, glove_word_vec_dict):
+	numNonZero = 0
+	vector = []
+	for word in finalList:
+		vector.append(getWordVector((word, glove_word_vec_dict)))
+	return vector
+
+def simplify(word):
+	dump = ''
+	temp = []
+	listOfWords = list(filter(None, re.split('([A-Z][^A-Z]*)', word)))
+	if len(listOfWords) == len(word):
+		return word.lower()
+	for i in range(len(listOfWords)):
+		listOfWords[i] = listOfWords[i].lower()
+		if len(listOfWords[i]) == 1:
+			dump = dump + listOfWords[i]
+			if dump in words.words() and len(dump) > 2:
+				temp.append(dump)
+				dump = ''
+		else:
+			temp.append(listOfWords[i])
+
+	return temp
+
+def glove(glove_word_vec_dict, trainComments):
+	def createTokens(data, glove_word_vec_dict):
+		listofComments = []
+		listofStances = []
+		CommentVector = []
+		for ind, row in data.iterrows():
+			#create a sentence using target and comment. it will be used to form wordvector
+			example_sentence = 'Transsexual' + " " + str(row['Text'])
+			#remove punctuation
+			final_sentence = example_sentence.translate(string.punctuation)
+			wordList = word_tokenize(final_sentence)
+			finalList = []
+			s = ' '.join([i for i in wordList if i.isalpha()])
+			#create tokens from the string and stem them
+			wordList = word_tokenize(s)
+			wordList = [w.lower() for w in wordList]
+			stop_words = set(stopwords.words('german'))
+			wordList = [w for w in wordList if w not in stop_words]
+			for word in wordList:
+				#to break any combined word into its simplified components (e.g. hashtags)
+				finalList += simplify(word)
+			final_sentence = ' '.join(finalList)
+			listofComments.append(finalList)
+			listofStances.append(row['Label'])
+			CommentVector.append(sumVectors(finalList, glove_word_vec_dict))
+		return listofComments, listofStances, CommentVector
+		#remove punctuation from and tokenize the comments
+
+	listofComments, listofStances, trainCommentVector = createTokens(trainComments, glove_word_vec_dict)
+
+
+	from sklearn.model_selection import train_test_split
+	X_train, X_test, y_train, y_test = train_test_split(trainCommentVector, listofStances, test_size= 0.1, random_state = 42)
+
+	Xtrain = np.asarray(X_train)
+	X_test = np.asarray(X_test)
+	ytrain = np.asarray(y_train)
+	ytest = np.asarray(y_test)
+
+	return X_train, y_train, X_test, y_test
+
+totalAcc = []
+
+for classifier in classifiers:
+	print( 'The machine learning model used for classification: ' + classifier )
+	temp = []
+	#['Support Vector Machine', 'Random Forest Classifier', 'Gradient Boosting Classifier',  'K Neighbors Classifier', 'Decision Tree Classifier']
+	X_train, X_test, y_train, y_test = glove(glove_word_vec_dict, Comments)
+
+	if classifier == 'Support Vector Machine':
+		clf = SVC(kernel = 'rbf').fit(X_train, y_train)
+
+	elif classifier == 'Random Forest Classifier':
+		clf = RandomForestClassifier(n_estimators= 90).fit(X_train, y_train)
+
+	elif classifier == 'Gradient Boosting Classifier':
+		clf = GradientBoostingClassifier().fit(X_train, y_train)
+
+	elif classifier == 'K Neighbors Classifier':
+		clf = GaussianNB().fit(X_train, y_train)
+
+	elif classifier == 'Decision Tree Classifier':
+		clf = tree.DecisionTreeClassifier().fit(X_train, y_train)
+
+	acc = clf.score(X_test, y_test)
+
+	print("Total Test Accuracy is " + str(round(acc * 100, 2)) + "%")
+	totalAcc.append(acc)
+
+
+import matplotlib.pyplot as plt
+
+x  = ['SVM', 'RFC', 'GBC', 'KNN', 'DT']
+y  = totalAcc
+
+plt.plot(x, y)
+plt.plot()
+
+plt.xlabel("Models")
+plt.ylabel("Accuracy")
+plt.title("Comparison of Total Test Accuracy of different Baseline Models")
+plt.show()
